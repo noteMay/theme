95ebfc6abd361089d986e0c602caeb92f09e987a
May <122172232+noteMay@users.noreply.github.com>
2024-01-10 21:42:44 +0800

    Update footer.php

--- book/footer.php
+++ book/footer.php
@@ -3,7 +3,13 @@
         </div><!-- end .row -->
     </div>
 </div><!-- end #body -->
-
+<div class="site-search col-3 kit-hidden-tb container">
+                <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
+                    <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
+                    <input type="text" id="s" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>"/>
+                    <button type="submit" class="submit"><?php _e('搜索'); ?></button>
+                </form>
+            </div>
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动'); ?>.
